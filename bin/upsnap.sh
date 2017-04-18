a93e6047884f3a4479bcffd40cf8eb34530aebac
Anthony J. Bentley <anthony@anjbe.name>
2017-04-18 01:26:27 -0600

    Read /etc/installurl to determine mirror.

--- bin/upsnap.sh
+++ bin/upsnap.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 cd $(mktemp -d)
-ftp https://ftp3.usa.openbsd.org/pub/OpenBSD/snapshots/$(uname -m)/{bsd.rd,SHA256{.sig,}}
+if [ -f /etc/installurl ]; then
+	< /etc/installurl read mirror
+else
+	mirror=https://ftp3.usa.openbsd.org/pub/OpenBSD
+fi
+ftp "$mirror"/snapshots/$(uname -m)/{bsd.rd,SHA256{.sig,}}
 key1=$(ls /etc/signify/openbsd-??-base.pub | tail -2 | head -1)
 key2=$(ls /etc/signify/openbsd-??-base.pub | tail -2 | tail -1)
 (signify -C -p $key1 -x SHA256.sig bsd.rd  ||
