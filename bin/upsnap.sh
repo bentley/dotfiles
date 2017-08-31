f7e2e755d01d8314cfa2550f74792b3da1c5d4cd
Anthony J. Bentley <anthony@anjbe.name>
2017-08-31 00:04:09 -0600

    upsnap.sh: Exit early if fetching fails.

--- bin/upsnap.sh
+++ bin/upsnap.sh
@@ -6,7 +6,7 @@ if [ -f /etc/installurl ]; then
 else
 	mirror=https://ftp3.usa.openbsd.org/pub/OpenBSD
 fi
-ftp "$mirror"/snapshots/$(uname -m)/{bsd.rd,SHA256{.sig,}}
+ftp "$mirror"/snapshots/$(uname -m)/{bsd.rd,SHA256{.sig,}} || exit 1
 key1=$(ls /etc/signify/openbsd-??-base.pub | tail -2 | head -1)
 key2=$(ls /etc/signify/openbsd-??-base.pub | tail -2 | tail -1)
 (signify -C -p $key1 -x SHA256.sig bsd.rd  ||
