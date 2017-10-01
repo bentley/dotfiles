c7c075c9c656fa0774bcd1bcd3b622b95fa16431
Anthony J. Bentley <anthony@anjbe.name>
2017-09-30 20:42:22 -0600

    upsnap.sh: Get OpenBSD version from mirror rather than local system.

--- bin/upsnap.sh
+++ bin/upsnap.sh
@@ -6,9 +6,8 @@ if [ -f /etc/installurl ]; then
 else
 	mirror=https://ftp3.usa.openbsd.org/pub/OpenBSD
 fi
-ftp "$mirror"/snapshots/$(uname -m)/{bsd.rd,SHA256{.sig,}} || exit 1
-key1=$(ls /etc/signify/openbsd-??-base.pub | tail -2 | head -1)
-key2=$(ls /etc/signify/openbsd-??-base.pub | tail -2 | tail -1)
-(signify -C -p $key1 -x SHA256.sig bsd.rd  ||
- signify -C -p $key2 -x SHA256.sig bsd.rd) || exit 1
+ftp "$mirror"/snapshots/$(uname -m)/{bsd.rd,SHA256.sig} || exit 1
+V=$(grep man SHA256* | sed -e 's/SHA256//' -e 's/\.tgz.*//' -e 's/[^0-9]//g')
+key=$(ls /etc/signify/openbsd-$V-base.pub || exit 1)
+signify -C -p $key -x SHA256.sig bsd.rd || exit 1
 mv bsd.rd /
